8e2a2b00d05d27d7ab5fe1259309defe3f459bf4
a-thompson19 <57600271+a-thompson19@users.noreply.github.com>
2019-11-10 23:52:19 +0000

    Add files via upload

---
+++ DS Capstone wk1.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <h1>IBM Applied Data Science Capstone<h1>
+# 
+# <h2>This is a notebook for the Applied Data Science Capstone Project<h2>
+
+# In[2]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[9]:
+
+
+print ("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
